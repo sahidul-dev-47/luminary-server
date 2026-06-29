60f275e31040ea5fbccfa863f8fc1fa8abee1d01
saiful-100 <sahidulx47@gmail.com>
2026-06-29 22:44:41 +0600

    added payment method

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require('cors');
+const Stripe = require('stripe');
+
 const app = express();
 require('dotenv').config();
 
@@ -10,6 +12,7 @@ const port = 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = process.env.MONGODB_URI;
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,6 +29,8 @@ async function run() {
     
  const database = client.db("luminary_db");
  const ebooksCollection = database.collection('ebooks')
+ const userCollection = database.collection('user')
+ const transactionsCollection = database.collection('transaction')
 
 
  app.get('/api/ebooks', async(req, res) => {
@@ -70,6 +75,75 @@ app.post('/api/ebooks', async(req,res) => {
   const result = await ebooksCollection.insertOne(newEbook);
   res.send(result);
 })
+
+// transaction 
+
+
+app.post('/api/v1/payments/verify-status', async (req, res) => {
+  try {
+    const { session_id } = req.body;
+    
+    if (!session_id) {
+      return res.status(400).json({ message: "Session ID is required" });
+    }
+
+    const session = await stripe.checkout.sessions.retrieve(session_id);
+
+    if (session.payment_status === 'paid') {
+      const { ebookId, buyerEmail, price } = session.metadata;
+
+      // Duplicate check
+      const isAlreadyProcessed = await transactionsCollection.findOne({ 
+        transactionId: session.id 
+      });
+
+      if (isAlreadyProcessed) {
+        return res.status(200).json({ success: true, message: "Already processed." });
+      }
+
+      // Insert Transaction
+      const transactionInfo = {
+        transactionId: session.id,
+        ebookId,
+        buyerEmail,
+        amount: parseFloat(price),
+        paymentStatus: 'paid',
+        createdAt: new Date()
+      };
+      
+      await transactionsCollection.insertOne(transactionInfo);
+
+      // Update Ebook Status
+      await ebooksCollection.updateOne(
+        { $or: [{ _id: ebookId }, { _id: new ObjectId(ebookId) }] },
+        { $set: { status: 'Sold' } }
+      );
+
+      // Update User
+      await userCollection.updateOne(
+        { email: buyerEmail },
+        { 
+          $set: { 
+            lastPurchaseAt: new Date(),
+            
+          },
+          $push: { purchasedEbooks: ebookId }
+        }
+      );
+
+      return res.status(200).json({ 
+        success: true, 
+        message: "Payment verified successfully." 
+      });
+
+    } else {
+      return res.status(400).json({ success: false, message: 'Payment not completed.' });
+    }
+  } catch (error) {
+    console.error("Verification Error:", error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+});
  
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -84,6 +158,8 @@ run().then(() => {
   });
 }).catch(console.dir);
 
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+  
